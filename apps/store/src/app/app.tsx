b7cc703257db47866cc232cd4e53c96d22851b19
José Cruz <josecruz_siscomp@outlook.com>
2022-04-25 01:50:58 -0500

    coding master usign header component in store app

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -6,34 +6,38 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import { getAllGames } from '../fake-api';
+import { Header } from '@nxegghead/store/ui-shared';
 
 export function App() {
   return (
-    <div className='container'>
-      <div className='games-layout'>
-        {getAllGames().map((boardGame) => (
-          <Card key={boardGame.id} className="game-card">
-            <CardActionArea>
-              <CardMedia
-                className='game-card-media'
-                image={boardGame.image}
-                title={boardGame.name} />
-              <CardContent className=''>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {boardGame.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {boardGame.description}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p" className="game-rating">
-                  <strong>Rating: </strong> {boardGame.rating}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+    <>
+      <Header/>
+      <div className='container'>
+        <div className='games-layout'>
+          {getAllGames().map((boardGame) => (
+            <Card key={boardGame.id} className="game-card">
+              <CardActionArea>
+                <CardMedia
+                  className='game-card-media'
+                  image={boardGame.image}
+                  title={boardGame.name} />
+                <CardContent className=''>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {boardGame.name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {boardGame.description}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p" className="game-rating">
+                    <strong>Rating: </strong> {boardGame.rating}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
