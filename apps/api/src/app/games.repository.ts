3d4677585d8b87edb2018f3c6ca61db51fd0cc29
José Cruz <josecruz_siscomp@outlook.com>
2022-04-29 14:35:02 -0500

    coding master created new express application called api

---
+++ apps/api/src/app/games.repository.ts
@@ -0,0 +1,29 @@
+const games = [
+  {
+      id: 'brew',
+      name: 'Brew',
+      image: '/assets/brew.png',
+      description: 'Make potis',
+      price: 45,
+      rating: Math.random()
+  },
+  {
+      id: 'hues-and-cues',
+      name: 'Hues and Cues',
+      image: '/assets/hues_and_cues.jpeg',
+      description: 'Guess the color',
+      price: 23,
+      rating: Math.random()
+  },
+  {
+      id: 'coup',
+      name: 'Coup',
+      image: '/assets/coup.jpg',
+      description: 'Lie, kill and win',
+      price: 35,
+      rating: Math.random()
+  },
+];
+
+export const getAllGames = () => games;
+export const getGame = (id: string) => games.find((game) => game.id === id);
